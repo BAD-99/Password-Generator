8d0e16d4c8d9b75a127eef874752c3f87e7c9ab2
BAD-99 <brandon.andrew.davis@gmail.com>
2022-09-18 23:05:35 -0500

    Changes to css and html

---
+++ script.js
@@ -0,0 +1,6 @@
+const lowerCaseString = 'abcdefghijklmnopqrstuvwxyz'
+const upperCaseString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const numberString = '0123456789'
+const symbolString = '!@#$%^&*()-_=+[{]};:\'"\\|<,>./?'
+
+document.getElementById('character-length-output').innerText = symbolString
